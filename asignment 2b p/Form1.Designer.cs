27140df62827d68e322d5dc5f5a74ce6b7f41c63
Stephens <rc0034542@student.riversidecollege.ac.uk>
2019-10-21 15:32:12 +0100

    Add project files.

---
+++ asignment 2b p/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace asignment_2b_p
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.R1 = new System.Windows.Forms.Label();
+            this.R2 = new System.Windows.Forms.Label();
+            this.R3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::asignment_2b_p.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(173, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(587, 429);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(693, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // R1
+            // 
+            this.R1.AutoSize = true;
+            this.R1.Location = new System.Drawing.Point(363, 203);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(21, 13);
+            this.R1.TabIndex = 2;
+            this.R1.Text = "R1";
+            this.R1.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // R2
+            // 
+            this.R2.AutoSize = true;
+            this.R2.Location = new System.Drawing.Point(483, 202);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(21, 13);
+            this.R2.TabIndex = 3;
+            this.R2.Text = "R2";
+            this.R2.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // R3
+            // 
+            this.R3.AutoSize = true;
+            this.R3.Location = new System.Drawing.Point(580, 202);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(21, 13);
+            this.R3.TabIndex = 4;
+            this.R3.Text = "R3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(366, 309);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(436, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(532, 309);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(583, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.R3);
+            this.Controls.Add(this.R2);
+            this.Controls.Add(this.R1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label R1;
+        private System.Windows.Forms.Label R2;
+        private System.Windows.Forms.Label R3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ asignment 2b p/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asignment_2b_p
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double R1, R2, R3, RT;
+
+            try
+            {
+                R1 = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect Input.");
+                R1 = 0.0;
+            }
+            try
+            {
+                R2 = double.Parse(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect Input.");
+                R2 = 0.0;
+            }
+            try
+            {
+                R3 = double.Parse(textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect Input.");
+                R3 = 0.0;
+            }
+            RT = 1.0 / (1.0 / R1 + 1.0 / R2 + 1.0 / R3);
+            label1.Text = "RT =" + RT;
+        }
+private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
